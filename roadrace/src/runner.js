570e426cc2f67163fd318da0ca04d7f113eea352
Logan Vincent <ldvsimp@gmail.com>
2021-06-04 10:29:46 -0600

    Complete testing for runner and add .skip to roadrace-test

--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,5 +1,23 @@
 class Runner {
-
+  constructor(name, age,) {
+    this.name = name;
+    this.age = age;
+    this.milesRun = 0;
+    this.fitness = 25;
+    this.completedRaces = [];
+  }
+runSomeMiles(milesNum) {
+  this.milesRun = this.milesRun + milesNum;
+  this.fitness = this.milesRun + this.fitness;
+}
+stretch() {
+  this.fitness = this.fitness + .5;
+}
+runRace(raceName, distance) {
+  this.completedRaces.push(raceName);
+  this.milesRun = this.milesRun + distance;
+  this.fitness = this.fitness + distance;
+}
 }
 
 module.exports = Runner;
